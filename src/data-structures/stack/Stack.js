a5416a53ca8e6d890594881a6d28cd38eb93710b
yang <504575307@qq.com>
2020-06-27 23:53:43 +0800

    stack

--- src/data-structures/stack/Stack.js
+++ src/data-structures/stack/Stack.js
@@ -9,35 +9,41 @@ export default class Stack {
    * @return {boolean}
    */
   isEmpty() {
-
+    return !this.linkedList.head;
   }
 
   /**
    * @return {*}
    */
   peek() {
-
+    const node = this.linkedList.tail;
+    return node ? node.value : null;
   }
 
   /**
    * @param {*} value
    */
   push(value) {
-
+    return this.linkedList.append(value);
   }
 
   /**
    * @return {*}
    */
   pop() {
-
+    const node = this.linkedList.deleteTail();
+    return node ? node.value : null;
   }
 
   /**
    * @return {*[]}
    */
   toArray() {
-
+    const arr = [];
+    while (!this.isEmpty()) {
+      arr.push(this.pop());
+    }
+    return arr;
   }
 
   /**
@@ -45,6 +51,9 @@ export default class Stack {
    * @return {string}
    */
   toString(callback) {
-
+    this.linkedList.reverse();
+    const res = this.linkedList.toString(callback);
+    this.linkedList.reverse();
+    return res;
   }
 }
